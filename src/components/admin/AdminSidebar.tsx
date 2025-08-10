63489aee60665dc7c6499baf4e7e88956f5febc3
ahmed-H20 <ahmedheshamahah8@gmail.com>
2025-08-11 02:50:51 +0300

    Edit Name

--- src/components/admin/AdminSidebar.tsx
+++ src/components/admin/AdminSidebar.tsx
@@ -58,7 +58,7 @@ const AdminSidebar = () => {
           <div className="w-8 h-8 bg-primary rounded-lg flex items-center justify-center">
             <Store className="h-5 w-5 text-white" />
           </div>
-          <span className="font-bold text-lg">طلاباتك ستور 🛍️</span>
+          <span className="font-bold text-lg">Hyper10 ستور 🛍️</span>
         </div>
       </div>
 
